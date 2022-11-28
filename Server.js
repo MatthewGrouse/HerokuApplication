c50f7363aa58c199e402c114af7a2d570bb6d45c
Matthew Grouse <72737644+MatthewGrouse@users.noreply.github.com>
2022-11-28 23:58:18 +0000

    update port

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 //setting up the path
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.port || 5000;
 
 const path = require("path");
 let publicPath = path.resolve(__dirname, "public");
